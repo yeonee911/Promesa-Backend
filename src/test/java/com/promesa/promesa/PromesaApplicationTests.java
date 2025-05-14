a657ee2c53207ea17ed16d51941bcd4fe4cf117c
mirupio <162397816+mirupio@users.noreply.github.com>
2025-05-14 21:38:23 +0900

    chore : 프로젝트 생성

---
+++ src/test/java/com/promesa/promesa/PromesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promesa.promesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
