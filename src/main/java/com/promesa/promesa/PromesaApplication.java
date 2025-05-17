0f3b24941186ab04071b339fefa079cf3ff4a9ec
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-05-18 01:18:27 +0900

    Revert "Revert "chore : 프로젝트 생성""

---
+++ src/main/java/com/promesa/promesa/PromesaApplication.java
@@ -0,0 +1,13 @@
+package com.promesa.promesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromesaApplication.class, args);
+	}
+
+}
