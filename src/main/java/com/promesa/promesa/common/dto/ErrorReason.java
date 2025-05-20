7414406f24ec70714fa9d9308d63fe229d341985
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-05-21 00:47:13 +0900

    refactor: ErrorResponse 생성자 리팩토링

--- src/main/java/com/promesa/promesa/common/dto/ErrorReason.java
+++ src/main/java/com/promesa/promesa/common/dto/ErrorReason.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class ErrorReason {
-    private final Integer status;   // 상태 코드
+    private final int status;   // 상태 코드
     private final String code;      // 도메인 + 상태코드
     private final String reason;    // 구체적 설명
 }
