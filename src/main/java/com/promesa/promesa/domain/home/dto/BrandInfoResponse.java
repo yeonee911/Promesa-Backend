2499df01e235ac1b787e0ee704833aa4175cfcf8
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-05-21 01:59:47 +0900

    refactor: BrandInfoResponse 정적 인스턴스 생성

--- src/main/java/com/promesa/promesa/domain/home/dto/BrandInfoResponse.java
+++ src/main/java/com/promesa/promesa/domain/home/dto/BrandInfoResponse.java
@@ -3,4 +3,9 @@ package com.promesa.promesa.domain.home.dto;
 public record BrandInfoResponse(
         String mainImageUrl,
         String brandStory
-) {}
+) {
+    public static final BrandInfoResponse INFO = new BrandInfoResponse(
+            "static/brand-image.jpg",
+            "도예과 학생들의 작품을 세상에 소개하는 플랫폼입니다."
+    );
+}
