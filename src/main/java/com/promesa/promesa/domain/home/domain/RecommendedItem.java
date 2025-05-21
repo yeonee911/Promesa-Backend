eb3deeb92031d4a1b59f95da4b389b56918d91f0
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-05-21 18:47:14 +0900

    feat: QueryDsl 설정

--- src/main/java/com/promesa/promesa/domain/home/domain/RecommendedItem.java
+++ src/main/java/com/promesa/promesa/domain/home/domain/RecommendedItem.java
@@ -4,7 +4,7 @@ import com.promesa.promesa.domain.item.domain.Item;
 import jakarta.persistence.*;
 
 @Entity
-public class RecommendedIem {
+public class RecommendedItem {
     @Id @GeneratedValue
     private Long id;
 
