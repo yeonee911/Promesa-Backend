3fb4236c4f4b1cc547d93c33e5f211ef278feabc
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-05-31 20:39:50 +0900

    refactor: controller어노테이션 수정

--- src/main/java/com/promesa/promesa/PromesaApplication.java
+++ src/main/java/com/promesa/promesa/PromesaApplication.java
@@ -2,7 +2,9 @@ package com.promesa.promesa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PromesaApplication {
 
