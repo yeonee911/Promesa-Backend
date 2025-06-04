aaf932848646483b064a61d589726c1c36d46cbd
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-06-05 02:53:43 +0900

    feat: 블루그린 무중단 배포를 위한 scripts 생성

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
+# scripts/switch.sh
 
-# Crawl current connected port of WAS
-CURRENT_PORT=$(cat /home/ubuntu/service_url.inc  | grep -Po '[0-9]+' | tail -1)
-TARGET_PORT=0
+# /home/ubuntu/app/service_url.inc 파일을 Blue(8081) → Green(8082)으로 교체
+echo "set \$service_url 127.0.0.1:8082;" | sudo tee /home/ubuntu/app/service_url.inc
 
-echo "> Nginx currently proxies to ${CURRENT_PORT}."
-
-# Toggle port number
-if [ ${CURRENT_PORT} -eq 8081 ]; then
-    TARGET_PORT=8082
-elif [ ${CURRENT_PORT} -eq 8082 ]; then
-    TARGET_PORT=8081
-else
-    echo "> No WAS is connected to nginx"
-    exit 1
-fi
-
-# Change proxying port into target port
-echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ubuntu/service_url.inc
-
-echo "> Now nginx proxies to ${TARGET_PORT}."
-
-# Reload nginx
-sudo service nginx reload
-
-echo "> Nginx reloaded."
-
+# nginx 설정 재적용
+sudo systemctl reload nginx
