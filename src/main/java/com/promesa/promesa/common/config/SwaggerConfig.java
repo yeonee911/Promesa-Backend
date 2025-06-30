50e3ec062d035a7ee2356b8112d9773a0eb2d577
mirupio <162397816+mirupio@users.noreply.github.com>
2025-07-01 01:16:37 +0900

    refactor: SwaggerConfig 수정

--- src/main/java/com/promesa/promesa/common/config/SwaggerConfig.java
+++ src/main/java/com/promesa/promesa/common/config/SwaggerConfig.java
@@ -1,9 +1,15 @@
 package com.promesa.promesa.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +18,8 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "프로메사 API 명세서",
                 description = "도예 전공자들의 작품을 전시하고 판매하는 플랫폼, 프로메사(Promesa)"
-        )
+        ),
+        security = @io.swagger.v3.oas.annotations.security.SecurityRequirement(name = "Authorization")
 )
 
 @Configuration
@@ -20,6 +27,20 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI();
+        Components components = new Components()
+                .addSecuritySchemes("Authorization", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .in(SecurityScheme.In.HEADER)
+                        .name("Authorization"));
+
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .components(components);
+    }
+
+    private SecurityRequirement securityRequirement() {
+        return new SecurityRequirement().addList("Authorization");
     }
 }
