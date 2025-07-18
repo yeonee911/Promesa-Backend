9ee15c3fccfca042f1e58be118d13d6ae218cbd5
mirupio <162397816+mirupio@users.noreply.github.com>
2025-07-18 23:58:21 +0900

    refactor: OrderItemRequest DTO에 주석 추가

--- src/main/java/com/promesa/promesa/domain/order/dto/OrderItemRequest.java
+++ src/main/java/com/promesa/promesa/domain/order/dto/OrderItemRequest.java
@@ -1,6 +1,10 @@
 package com.promesa.promesa.domain.order.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record OrderItemRequest(
-        Long itemId,   // type이 "SINGLE"이면 itemId, "CART"이면 cartId
-        int quantity   // (선택) CART는 무시해도 됨
+        @Schema(description = "type이 'SINGLE'이면 itemId, 'CART'이면 cartId (장바구니 ID)")
+        Long itemId,
+        @Schema(description = "'SINGLE'일 경우에만 사용됨. 'CART'일 경우 무시됨")
+        int quantity
 ) {}
