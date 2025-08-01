3fb99c5224fedcaedefcc6eb3fd8e86aaff73060
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-08-02 00:53:46 +0900

    fix: RequestMapping 중복 해결

--- src/main/java/com/promesa/promesa/common/api/ImageController.java
+++ src/main/java/com/promesa/promesa/common/api/ImageController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/items")
+@RequestMapping("/images")
 @RequiredArgsConstructor
 public class ImageController {
 
