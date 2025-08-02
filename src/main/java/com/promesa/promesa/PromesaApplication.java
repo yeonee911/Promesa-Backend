a953e5e0318c8894413f16fd7e9c5504bef5efcf
서채연 <128215774+yeonee911@users.noreply.github.com>
2025-08-02 21:20:24 +0900

    Update PromesaApplication.java

--- src/main/java/com/promesa/promesa/PromesaApplication.java
+++ src/main/java/com/promesa/promesa/PromesaApplication.java
@@ -2,6 +2,7 @@ package com.promesa.promesa;
 
 import com.promesa.promesa.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,7 +10,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
-@SpringBootApplication(exclude = SystemMetricsAutoConfiguration.class)
+@SpringBootApplication(
+		exclude = SystemMetricsAutoConfiguration.class
+)
 @EnableConfigurationProperties(JwtProperties.class)
 public class PromesaApplication {
 
